be3df3d8449cdbb736bdff0db7ae38468b77944d
LikhitaYerra <95490590+LikhitaYerra@users.noreply.github.com>
2025-02-05 11:40:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,17 @@
-# Copyright 2021 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import signal
 import sys
 from types import FrameType
-
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import trein_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import load_iris
+ 
 from flask import Flask
 
 from utils.logging import logger
-
-app = Flask(__name__)
-
 
 @app.route("/")
 def hello() -> str:
@@ -30,8 +20,15 @@ def hello() -> str:
 
     # https://cloud.google.com/run/docs/logging#correlate-logs
     logger.info("Child logger with trace Id.")
-
-    return "Hello, World!"
+    iris = load_iris()
+    X = iris.data[:,[2,3]]
+    y = iris.target
+    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.25,random_state = 42)
+    dt = DecisionTreeClassifier(max_depth=3,min_samples_leaf = 10,random_state=1)
+    dt.fit(X,y)
+    y_pred = dt.predict(X_test)
+    rmse = np.sqrt(mean_squared_error(y_test,y_pred))
+    return "prediction vs True:"+str(y_pred[0]+","+str(y_test[0]
 
 
 def shutdown_handler(signal_int: int, frame: FrameType) -> None:
